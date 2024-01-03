940946587441baa3ecb59dd87ee409200192e34a
Troy Comi <troycomi@gmail.com>
2024-01-03 12:49:26 -0500

    feat: add snakemake syntax to lazyvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+augroup snake_syn
+    autocmd!
+    autocmd Filetype snakemake set tabstop=4 shiftwidth=4 commentstring=#\ %s
+    autocmd Filetype snakemake syn keyword pythonBuiltinObj paths
+    autocmd Filetype snakemake highlight link pythonBuiltinObj Identifier
+    autocmd Filetype snakemake highlight link pythonBuiltinFunc Function
+    autocmd Filetype snakemake highlight link pythonBuiltinType Structure
+augroup end
+]])
--- .config/nvim/lua/plugins/basic.lua
+++ .config/nvim/lua/plugins/basic.lua
@@ -1,11 +1,17 @@
 return {
   { "sam4llis/nvim-lua-gf" },
-  { "rcarriga/nvim-notify", opts = {
-    top_down = false,
-  } },
-  { "folke/noice.nvim", opts = {
-    presets = { bottom_search = false },
-  } },
-  { "snakemake/snakemake", rtp = "misc/vim" },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      top_down = false,
+    }
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = { bottom_search = false },
+    }
+  },
+  { "snakemake/snakemake" },
   { "folke/tokyonight.nvim", opts = { style = "night" } },
 }
