e09c953805f88b11f1a24155ea9c927d4200eb61
Troy Comi <troycomi@gmail.com>
2024-02-01 12:37:11 -0500

    feat: unify time disaply in nvim and tmux

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    sections = {
+      lualine_z = {
+        function()
+          return " " .. os.date("%I:%M")
+        end,
+      },
+    },
+  },
+}
